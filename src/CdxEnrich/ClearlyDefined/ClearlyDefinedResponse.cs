b4ef72f2e6406ba1fc912bd827e19c76ec2ba042
Kai Lüttmann <kai.luettmann@smapone.com>
2025-06-06 15:02:31 +0200

    Refactor: Move ClearlyDefinedResponse to a separate file
    Relocated the ClearlyDefinedResponse class from ClearlyDefinedClient.cs to a new ClearlyDefinedResponse.cs file. This improves code organization and separation of concerns, making the project structure cleaner and easier to maintain.

---
+++ src/CdxEnrich/ClearlyDefined/ClearlyDefinedResponse.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CdxEnrich.ClearlyDefined
+{
+    public class ClearlyDefinedResponse
+    {
+        [JsonPropertyName("licensed")] public required LicensedData Licensed { get; init; }
+
+        public class LicensedData
+        {
+            [JsonPropertyName("facets")] public required Facets Facets { get; init; }
+        }
+
+        public class Facets
+        {
+            [JsonPropertyName("core")] public required Core Core { get; init; }
+        }
+
+        public class Core
+        {
+            [JsonPropertyName("discovered")] public required Discovered Discovered { get; init; }
+        }
+
+        public class Discovered
+        {
+            [JsonPropertyName("expressions")] public List<string>? Expressions { get; init; }
+        }
+    }
+}
